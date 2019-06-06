b66fe566c9d0120449f09244bf09e9a1e9a7d1b9
Tucker <trblackw@ncsu.edu>
2019-06-06 18:13:36 -0400

    convert components to typescript

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -31,13 +31,13 @@ const Layout: React.FC<{ children: ReactNode }> = ({ children }): JSX.Element =>
                   margin: `0 auto`,
                   maxWidth: 960,
                   padding: `0px 1.0875rem 1.45rem`,
-                  paddingTop: 0
+                  paddingTop: 0,
                }}>
                <main>{children}</main>
                <footer>
                   © {new Date().getFullYear()}, Built with
                   {` `}
-                  <a href='https://www.gatsbyjs.org'>Gatsby</a>
+                  <a href="https://www.gatsbyjs.org">Gatsby</a>
                </footer>
             </div>
          </Fragment>
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -5,10 +5,10 @@ import SEO from '../components/seo';
 
 const SecondPage: React.FC = (): JSX.Element => (
    <Layout>
-      <SEO title='Page two' />
+      <SEO title="Page two" />
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
-      <Link to='/'>Go back to the homepage</Link>
+      <Link to="/">Go back to the homepage</Link>
    </Layout>
 );
 
