988fbff134582efe644ab547c7668171d188f153
Tucker <trblackw@ncsu.edu>
2019-06-16 16:01:00 -0400

    add Link component

--- src/pages/posts/page-3.tsx
+++ src/pages/posts/page-3.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Layout from '../../components/layout';
+import { Link } from 'gatsby';
 
 export default (): JSX.Element => (
-   <div>
+   <Layout>
       <h1>Hello from page 3</h1>
-   </div>
+      <Link to='/page-2'>Go to page 2</Link>
+   </Layout>
 );
