b44916d715123badd5f63afd0a5c81d399cff1b3
Tucker <trblackw@ncsu.edu>
2019-06-19 17:55:15 -0400

    include author in meta data and extract query into variable

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,42 +6,45 @@
  */
 
 import React, { ReactNode, Fragment } from 'react';
-import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
 import './layout.css';
 
+const GET_SITE_METADATA = graphql`
+   {
+      site {
+         siteMetadata {
+            title
+            author
+         }
+      }
+   }
+`;
+
 const Layout: React.FC<{ children: ReactNode }> = ({ children }): JSX.Element => (
    <StaticQuery
-      query={graphql`
-         query SiteTitleQuery {
-            site {
-               siteMetadata {
-                  title
-               }
-            }
-         }
-      `}
-      render={(data: any): JSX.Element => (
-         <Fragment>
-            <Header siteTitle={data.site.siteMetadata.title} />
-            <div
-               style={{
-                  margin: `0 auto`,
-                  maxWidth: 960,
-                  padding: `0px 1.0875rem 1.45rem`,
-                  paddingTop: 0,
-               }}>
-               <main>{children}</main>
-               <footer>
-                  © {new Date().getFullYear()}, Built with
-                  {` `}
-                  <a href="https://www.gatsbyjs.org">Gatsby</a>
-               </footer>
-            </div>
-         </Fragment>
-      )}
+      query={GET_SITE_METADATA}
+      render={(data: any): JSX.Element => {
+         const { title, author } = data.site.siteMetadata;
+         return (
+            <Fragment>
+               <Header siteTitle={title} />
+               <div
+                  style={{
+                     margin: `0 auto`,
+                     maxWidth: 960,
+                     padding: `0px 1.0875rem 1.45rem`,
+                     paddingTop: 0
+                  }}>
+                  <main>{children}</main>
+                  <footer>
+                     © {new Date().getFullYear()}, Built by {author}
+                  </footer>
+               </div>
+            </Fragment>
+         );
+      }}
    />
 );
 
