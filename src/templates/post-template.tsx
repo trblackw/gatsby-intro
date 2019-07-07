fda1afee7ac04ca008e5c051cd69a2727d644334
Tucker <trblackw@ncsu.edu>
2019-07-07 18:13:10 -0400

    Add template for posts

---
+++ src/templates/post-template.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default (): JSX.Element => (
+   <Layout>
+      <h2>Programmatically created blog post!</h2>
+   </Layout>
+);
