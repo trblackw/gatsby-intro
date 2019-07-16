046aaf4d831dc9ac7070460d0160c59fd0fc0d69
Tucker <trblackw@ncsu.edu>
2019-07-16 18:08:44 -0400

    set up logic for convertng markdown for blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require('gatsby-source-filesystem');
+const path = require('path');
+const PostTemplate = path.resolve('./src/templates/post-template.tsx');
 
-// You can delete this file if you're not using it
+//programmatically generate slugs for posts & their respective pages
+const MarkdownRemark = 'MarkdownRemark';
+exports.onCreateNode = ({ node, getNode, actions }) => {
+   if (node.internal.type === MarkdownRemark) {
+      const slug = createFilePath({ node, getNode, basePath: 'posts' });
+      actions.createNodeField({
+         node,
+         name: 'slug',
+         value: slug
+      });
+   }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+   const { data } = await graphql`
+      {
+         allMarkdownRemark {
+            edges {
+               node {
+                  fields {
+                     slug
+                  }
+               }
+            }
+         }
+      }
+   `;
+   const posts = data.allMarkdownRemark.edges;
+   posts.forEach(({ node: post }) =>
+      actions.createPage({
+         path: `posts${post.field.slug}`,
+         component: PostTemplate,
+         context: {
+            slug: post.fields.slug
+         }
+      })
+   );
+};
