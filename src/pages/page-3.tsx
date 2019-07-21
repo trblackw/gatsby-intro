b3a5772569262a1eabaeae250fb4382e6579f0d6
Tucker <trblackw@ncsu.edu>
2019-07-21 14:10:59 -0400

    Move page 3 out of posts directory and delete empty posts directory

--- src/pages/page-3.tsx
+++ src/pages/page-3.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../components/layout';
+import Layout from '../components/layout';
 import { Link, graphql, StaticQuery } from 'gatsby';
 
 const getImageData = graphql`
