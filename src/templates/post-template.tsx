08965ec72d03f7d6da772db8b776c81931415c5e
Tucker <trblackw@ncsu.edu>
2019-07-25 17:33:08 -0400

    Modify post-template to programmatically display posts/slugs

--- src/templates/post-template.tsx
+++ src/templates/post-template.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { graphql } from 'gatsby';
+import styled from 'styled-components';
 
-export default (): JSX.Element => (
+const PostTemplate = ({ data: post }: { data: any }): JSX.Element => (
    <Layout>
-      <h2>Programmatically created blog post!</h2>
+      <Container>
+         <PostTitle>{post.markdownRemark.frontmatter.title}</PostTitle>
+         <div dangerouslySetInnerHTML={{ __html: post.markdownRemark.html }}></div>
+      </Container>
    </Layout>
 );
+
+export const query = graphql`
+   query($slug: String!) {
+      markdownRemark(fields: { slug: { eq: $slug } }) {
+         html
+         frontmatter {
+            title
+         }
+      }
+   }
+`;
+
+export default PostTemplate;
+
+const Container = styled.div`
+   margin: 0 auto;
+`
+
+const PostTitle = styled.h1`
+   font-size: 1.5em;
+   font-weight: bolder;
+`
