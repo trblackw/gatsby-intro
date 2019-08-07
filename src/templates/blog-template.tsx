0051e423d0ee1bcc488e9d4925df4e2ef4903f02
Tucker <trblackw@ncsu.edu>
2019-08-07 18:46:13 -0400

    Add dynamic pagination for blog posts

--- src/templates/blog-template.tsx
+++ src/templates/blog-template.tsx
@@ -24,38 +24,68 @@ const getMarkdownPosts = graphql`
    }
 `;
 
-export default (): JSX.Element => (
-   <Layout>
-      <Container>
-         <BlogTitle>Gatsby Blog</BlogTitle>
-         <hr />
-         <StaticQuery
-            query={getMarkdownPosts}
-            render={({ allMarkdownRemark }): JSX.Element => (
-               <Fragment>
-                  <PostCount>{allMarkdownRemark.totalCount} Posts</PostCount>
-                  {allMarkdownRemark.edges.map(
-                     ({
-                        node: { frontmatter, id, excerpt, fields }
-                     }: {
-                        node: { frontmatter: any; id: string | number; excerpt: string; fields: { slug: string } };
-                     }): JSX.Element => (
-                        <PostContainer key={id}>
-                           <PostTitle>
-                              <StyledLink to={`/posts${fields.slug}`}>{frontmatter.title}</StyledLink>
-                              <BlogDate>{frontmatter.date}</BlogDate>
-                           </PostTitle>
-                           <Excerpt>{excerpt}</Excerpt>
-                        </PostContainer>
-                     )
-                  )}
-               </Fragment>
+export default ({ data, pageContext }: { data: any; pageContext: any }): JSX.Element => {
+   const { currentPage, isFirstPage, isLastPage } = pageContext;
+   const nextPage = `/blog/${String(currentPage + 1)}`;
+   const prevPage = currentPage - 1 === 1 ? '/blog' : `/blog/${currentPage - 1}`;
+   return (
+      <Layout>
+         <Container>
+            <BlogTitle>Gatsby Blog</BlogTitle>
+            <hr />
+            <PostCount>{data.allMarkdownRemark.totalCount} Posts</PostCount>
+            {data.allMarkdownRemark.edges.map(
+               ({
+                  node: { frontmatter, id, excerpt, fields }
+               }: {
+                  node: { frontmatter: any; id: string | number; excerpt: string; fields: { slug: string } };
+               }): JSX.Element => (
+                  <PostContainer key={id}>
+                     <PostTitle>
+                        <StyledLink to={`/posts${fields.slug}`}>{frontmatter.title}</StyledLink>
+                        <BlogDate>{frontmatter.date}</BlogDate>
+                     </PostTitle>
+                     <Excerpt>{excerpt}</Excerpt>
+                  </PostContainer>
+               )
             )}
-         />
-      </Container>
-   </Layout>
-);
+            <div>
+               {!isFirstPage && (
+                  <Link to={prevPage} rel='prev'>
+                     Prev Page
+                  </Link>
+               )}
+               {!isLastPage && (
+                  <Link to={nextPage} rel='next'>
+                     Next Page
+                  </Link>
+               )}
+            </div>
+         </Container>
+      </Layout>
+   );
+};
 
+export const query = graphql`
+   query($skip: Int!, $limit: Int!) {
+      allMarkdownRemark(skip: $skip, limit: $limit) {
+         totalCount
+         edges {
+            node {
+               fields {
+                  slug
+               }
+               id
+               frontmatter {
+                  title
+                  date
+               }
+               excerpt
+            }
+         }
+      }
+   }
+`;
 const StyledLink = styled(Link)`
    text-decoration: none;
    color: #5D2392;
@@ -63,7 +93,7 @@ const StyledLink = styled(Link)`
    &:hover {
       text-decoration: underline;
    }
-`
+`;
 
 const Container = styled.div``;
 
